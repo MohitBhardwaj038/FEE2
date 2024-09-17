40798a8cefe610f2083555cb57a469cc44f73d41
MohitBhardwaj038 <mohitsharma93165@gmail.com>
2024-09-17 21:27:22 +0530

    update

---
+++ Class work/lec 20/variable.js
@@ -0,0 +1,34 @@
+// // redecleration and assignment both possible
+// var a = 10;
+// console.log(a);
+// var a = 20;
+// console.log(a);
+
+
+// // redeclaration not possible and assignment is also not possible in const
+// const a = 30;
+// console.log(a);
+
+
+let a=10;
+let b=34;
+let c=2.4;
+console.log(a);
+console.log(b);
+console.log(c);
+
+let name="shiv";
+console.log(name);
+
+
+
+let x=true;
+console.log(typeof(x));
+
+let y;
+console.log(typeof(y));
+
+
+
+let j=null;
+console.log(typeof(j));
